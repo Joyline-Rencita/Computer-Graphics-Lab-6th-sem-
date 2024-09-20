1a0a487e21f0f421ba745457cc4e39492d1482c1
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-09-20 10:13:43 +0530

    Update 03_Cube_Spin.c

--- 03_Cube_Spin.c
+++ 03_Cube_Spin.c
@@ -1,115 +1,82 @@
-#include<windows.h>
-#include<GL/glu.h>
 #include<GL/glut.h>
 
-GLfloat d = 0;
-int a=0;
+float ver[][3]={{0,0,0},{1,0,0},{1,1,0},{0,1,0},{0,0,1},{1,0,1},{1,1,1},{0,1,1}};
+float theta[]={0,0,0};
+int axis=0;
 
-void MyInit()
-{
-    glClearColor(0,0,0,1);
-    glEnable(GL_DEPTH_TEST);
-}
-
-void Spin()
-{
-    d = d + 0.25;
-    if(d > 360)
-        d = 0;
-    glutPostRedisplay();
-}
-
-void Face(GLfloat A[],GLfloat B[],GLfloat C[],GLfloat D[])
+void polygon(int a,int b,int c,int d)
 {
     glBegin(GL_POLYGON);
-        glVertex3fv(A);
-        glVertex3fv(B);
-        glVertex3fv(C);
-        glVertex3fv(D);
+    glColor3fv(ver[a]);
+    glVertex3fv(ver[a]);
+    glColor3fv(ver[b]);
+    glVertex3fv(ver[b]);
+    glColor3fv(ver[c]);
+    glVertex3fv(ver[c]);
+    glColor3fv(ver[d]);
+    glVertex3fv(ver[d]);
     glEnd();
 }
-
-void Cube(GLfloat V0[],GLfloat V1[],GLfloat V2[],GLfloat V3[],GLfloat V4[],GLfloat V5[],GLfloat V6[],GLfloat V7[])
+void ColorCube(void)
 {
-    glColor3f(1,0,0);
-    Face(V0,V1,V2,V3); //Front
-    glColor3f(0,1,0);
-    Face(V4,V5,V6,V7); //Back
-    glColor3f(0,0,1);
-    Face(V0,V4,V7,V3); //Left
-    glColor3f(1,1,0);
-    Face(V1,V5,V6,V2); //Right
-    glColor3f(1,0,1);
-    Face(V2,V3,V7,V6); //Bot
-    glColor3f(0,1,1);
-    Face(V0,V1,V5,V4); //Top
+    polygon(0,3,2,1);
+    polygon(2,3,7,6);
+    polygon(0,4,7,3);
+    polygon(1,2,6,5);
+    polygon(4,5,6,7);
+    polygon(0,1,5,4);
 }
 
-void Draw()
+void display()
 {
-    GLfloat V[8][3] =   {
-                            {-0.5, 0.5, 0.5},
-                            { 0.5, 0.5, 0.5},
-                            { 0.5,-0.5, 0.5},
-                            {-0.5,-0.5, 0.5},
-                            {-0.5, 0.5,-0.5},
-                            { 0.5, 0.5,-0.5},
-                            { 0.5,-0.5,-0.5},
-                            {-0.5,-0.5,-0.5},
-                        };
-    GLfloat rV[8][3],r;
-    int i;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    r = d*3.14/180;
-
-    if(a == 1)
-    {
-        for(i=0;i<8;i++)
-        {
-            rV[i][0] = V[i][0];
-            rV[i][1] = V[i][1]*cos(r)-V[i][2]*sin(r);
-            rV[i][2] = V[i][1]*sin(r)+V[i][2]*cos(r);
-        }
-    }
-
-    if(a == 2)
-    {
-        for(i=0;i<8;i++)
-        {
-            rV[i][0] = V[i][2]*sin(r)+V[i][0]*cos(r);
-            rV[i][1] = V[i][1];
-            rV[i][2] = V[i][2]*cos(r)-V[i][0]*sin(r);
-        }
-    }
-
-    if(a == 3)
-    {
-        for(i=0;i<8;i++)
-        {
-            rV[i][0] = V[i][0]*cos(r)-V[i][1]*sin(r);
-            rV[i][1] = V[i][0]*sin(r)+V[i][1]*cos(r);
-            rV[i][2] = V[i][2];
-        }
-    }
-
-    Cube(rV[0],rV[1],rV[2],rV[3],rV[4],rV[5],rV[6],rV[7]);
-
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glRotatef(theta[0],1,0,0);
+    glRotatef(theta[1],0,1,0);
+    glRotatef(theta[2],0,0,1);
+    ColorCube();
+    glFlush();
     glutSwapBuffers();
 }
-
-int main(int argc, char *argv[])
+void SpinCube()
 {
-    printf("Enter the Axis of Rotation [ 1->Xaxis | 2->Yaxis | 3->Zaxis ]: ");
-    scanf("%d",&a);
+    theta[axis]+=0.5;
+    if(theta[axis]>360)
+        theta[axis]=0;
+    display();
+}
+void mouse(int btn,int state,int x,int y)
+{
+    if(btn=GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+        axis=0;
+    if(btn=GLUT_MIDDLE_BUTTON && state==GLUT_DOWN)
+        axis=1;
+    if(btn=GLUT_RIGHT_BUTTON && state==GLUT_DOWN)
+        axis=2;
+
+}
+void myReshape(int w,int h)
+
+{
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(w<=h)
+        glOrtho(-2,2,-2*h/w,2*h/w,-10,10);
+    else
+        glOrtho(-2*h/w,2*h/w,-2,2,-10,10);
+        glMatrixMode(GL_MODELVIEW);
+}
+void main(int argc,char **argv)
+{
     glutInit(&argc,argv);
-    glutInitWindowSize(600,600);
-    glutInitWindowPosition(50,150);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("Cube Spin with Matrices");
-    MyInit();
-    glutDisplayFunc(Draw);
-    glutIdleFunc(Spin);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("ROTATING COLOR CUBE: C Tathva");
+    glutReshapeFunc(myReshape);
+    glutDisplayFunc(display);
+    glutIdleFunc(SpinCube);
+    glutMouseFunc(mouse);
+    glEnable(GL_DEPTH_TEST);
     glutMainLoop();
-    return 0;
 }
